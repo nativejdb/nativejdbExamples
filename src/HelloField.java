72f372c364931bf1214869a05fd8189daec3fbdf
aavarghese <avarghese@us.ibm.com>
2022-03-24 09:08:11 -0400

    Run configs
    Signed-off-by: aavarghese <avarghese@us.ibm.com>

--- src/HelloField.java
+++ src/HelloField.java
@@ -6,22 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HelloField {
+    public static int INT_FIELD = 3;
+    protected static final String STRING_FIELD = "QUARKUS";
 
-    public static String INT_FIELD = "intField";
-    public static final String LONG_FIELD = "longField";
-    protected final String String_Field = "Fixed Value";
-
-    private char charField;
-    private double doubleField;
-    private String stringField;
-
-    private int[] arrayField;
-    private List listField = new ArrayList<>();
+    private static int[] arrayField;
+    private static List listField = new ArrayList<String>();
 
     public static void main(String[] args){
         try {
             Thread.sleep(10000);
 
+            arrayField = new int[2];
+            arrayField[0] = INT_FIELD;
+            listField.add(STRING_FIELD);
+
+            String print = listField.get(0) + "is fun x " + arrayField.length;
+
+            System.out.println(print);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
