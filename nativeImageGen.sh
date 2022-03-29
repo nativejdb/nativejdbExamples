428b57c738c395d5364ae2bd5483522cf11af73a
aavarghese <avarghese@us.ibm.com>
2022-03-29 09:24:51 -0400

    Updates
    Signed-off-by: aavarghese <avarghese@us.ibm.com>

--- nativeImageGen.sh
+++ nativeImageGen.sh
@@ -10,19 +10,12 @@ done
 
 cp -r src/* apps/
 javac apps/$CLASS_NAME/*.java
-jar -cfe apps/$CLASS_NAME.jar $CLASS_NAME/$CLASS_NAME apps/$CLASS_NAME/*.class
 
 IMAGE_NAME=debugeeImg
 
 # Usage: native-image [options] class [imagename] [options]
 #    or  native-image [options] -jar jarfile [imagename] [options]
-cd /jdwp/apps && $GRAALVM_HOME/bin/native-image -g -O0 -H:-OmitInlinedMethodDebugLineInfo -cp $CLASS_NAME/* -jar $CLASS_NAME.jar $IMAGE_NAME
-
-
-for (( ; ; ))
-do
- sleep 2
-done
+cd /jdwp/apps && jar -cfe $CLASS_NAME.jar $CLASS_NAME/$CLASS_NAME $CLASS_NAME/*.class && $GRAALVM_HOME/bin/native-image -g -O0 -H:-OmitInlinedMethodDebugLineInfo -cp $CLASS_NAME.jar $CLASS_NAME.$CLASS_NAME $IMAGE_NAME
 
 #Exit with status of process
 exit $?
