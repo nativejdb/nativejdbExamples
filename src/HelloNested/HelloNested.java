018fc774b5a4197685e28aaed392dd97d5dca7b5
Mandana Vaziri <mvaziri@us.ibm.com>
2022-03-29 14:53:32 -0400

    minor changes in HelloNested

--- src/HelloNested/HelloNested.java
+++ src/HelloNested/HelloNested.java
@@ -13,40 +13,28 @@ public class HelloNested {
             } else {
                 throw new RuntimeException("zero or one args expected");
             }
-
         }
-
         public abstract void greet();
-
     }
-
-
     public static class DefaultGreeter extends Greeter {
         public void greet() {
             System.out.println("Hello.Hello World!");
-
         }
-
     }
-
     public static class NamedGreeter extends Greeter {
         private String name;
 
-        public NamedGreeter(String name) {
-            this.name = name;
-
-        }
+        public NamedGreeter(String name) { this.name = name; }
 
         public void greet() {
             System.out.println("Hello " + name);
         }
-
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Thread.sleep(10000);
         Greeter greeter = Greeter.greeter(args);
         greeter.greet();
+        Thread.sleep(10000);
     }
 }
 
